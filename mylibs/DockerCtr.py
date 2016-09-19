d6a66cc471bcf7d6e3b55365a36e71c5db9ad4df
sawanoboly <sawanoboriyu@higanworks.com>
2016-09-20 00:29:29 +0900

    change order of methods

--- mylibs/DockerCtr.py
+++ mylibs/DockerCtr.py
@@ -115,129 +115,6 @@ class DockerCtr:
                 query['serviceType'] = 'edit-wordpress'
         return body
 
-    def __getSyncEfsToS3ImageBody(self, query):
-        self.uuid = uuid.uuid4().hex
-        dynamodb = DynamoDB()
-        dbData = dynamodb.getServiceById(query['siteId'])
-        dbItem = False
-        if 'Items' in dbData:
-            if (dbData['Count'] > 0):
-                dbItem = dbData['Items'][0]
-        if dbItem is False:
-            dbItem = {
-                's3_bucket': {'S': ''},
-                's3_region': {'S': ''},
-            }
-
-        body = {
-                "Name": self.uuid,
-                "Labels": {
-                    "Name": "sync-efs-to-s3"
-                },
-                "TaskTemplate": {
-                    "RestartPolicy": {
-                        "Condition": "on-failure",
-                        "Delay": 5000,
-                        "MaxAttempts": 3,
-                    },
-                    "ContainerSpec": {
-                        "Image": self.__getImage('sync-efs-to-s3'),
-                        "Env": [
-                            "AWS_ACCESS_KEY_ID=" + self.app_config['awscreds']['access_key'],
-                            "AWS_SECRET_ACCESS_KEY=" + self.app_config['awscreds']['secret_access_key'],
-                            "S3_REGION=" + dbItem['s3_region']['S'],
-                            "S3_BUCKET=" + dbItem['s3_bucket']['S'],
-                            "SITE_ID=" + query['siteId'],
-                            "SERVICE_NAME=" + self.uuid
-                        ],
-                        "Mounts": [{
-                            "Type": "volume",
-                            "Target": "/opt/efs/",
-                            "Source": query['fsId'] + "/" + query['siteId'],
-                            "VolumeOptions": {
-                                "DriverConfig": {
-                                    "Name": "efs"
-                                }
-                            }
-                        }]
-                    },
-                    "Placement": {
-                        "Constraints": ["node.labels.type == efs-worker"]
-                    },
-                }
-            }
-        return body
-
-    def __getWpServiceImageBody(self, query):
-        if 'phpVersion' not in query:
-            query['phpVersion'] = '7.0'
-        s3 = S3(self.app_config)
-        notification_url = s3.createNotificationUrl(self.notificationId)
-        env = [
-            "SERVICE_PORT=" + str(query['pubPort']),
-            "SITE_ID=" + query['siteId'],
-            "SERVICE_DOMAIN=" + self.app_config['service_domain'],
-            "EFS_ID=" + query['fsId'],
-            "NOTIFICATION_URL=" + base64.b64encode(notification_url)
-        ]
-        if 'wpArchiveId' in query:
-            archiveUrl = s3.createWpArchiceUrl(query['wpArchiveId'])
-            if archiveUrl is not False:
-                env.append('ARCHIVE_URL=' + base64.b64encode(archiveUrl))
-        body = {
-                "Name": query['siteId'],
-                "Labels": {
-                    "Name": "wordpress-worker"
-                },
-                "TaskTemplate": {
-                    "LogDriver": {
-                        "Name": "awslogs",
-                        "Options": {
-                            "awslogs-region": "us-east-1",
-                            "awslogs-group": "dockerlog-services",
-                            "awslogs-stream": query['siteId']
-                        }
-                    },
-                    "ContainerSpec": {
-                        "Image": self.__getImage('wordpress-worker', query['phpVersion']),
-                        "Env": env,
-                        "Mounts": [{
-                            "Type": "volume",
-                            "Target": "/var/www/html",
-                            "Source": query['fsId'] + "/" + query['siteId'] + "/web",
-                            "VolumeOptions": {
-                                "DriverConfig": {
-                                    "Name": "efs"
-                                }
-                            }
-                        },
-                        {
-                            "Type": "volume",
-                            "Target": "/var/lib/mysql",
-                            "Source": query['fsId'] + "/" + query['siteId'] + "/db",
-                            "VolumeOptions": {
-                                "DriverConfig": {
-                                    "Name": "efs"
-                                }
-                            }
-                        }]
-                    },
-                    "Placement": {
-                        "Constraints": ["node.labels.type == efs-worker"]
-                    },
-                },
-                "EndpointSpec": {
-                    "Ports": [
-                        {
-                            "Protocol": "tcp",
-                            "PublishedPort": int(query['pubPort']),
-                            "TargetPort": 443
-                        }
-                    ]
-                }
-        }
-        return body
-
     def getTheService(self, siteId):
         try:
             res = self.docker_session.get(self.dockerapi_config['endpoint'] + 'services/' + siteId)
@@ -394,3 +271,126 @@ class DockerCtr:
 
     def deleteServiceByServiceId(self, query):
         return deleteTheService(query['serviceId'])
+
+    def __getSyncEfsToS3ImageBody(self, query):
+        self.uuid = uuid.uuid4().hex
+        dynamodb = DynamoDB()
+        dbData = dynamodb.getServiceById(query['siteId'])
+        dbItem = False
+        if 'Items' in dbData:
+            if (dbData['Count'] > 0):
+                dbItem = dbData['Items'][0]
+        if dbItem is False:
+            dbItem = {
+                's3_bucket': {'S': ''},
+                's3_region': {'S': ''},
+            }
+
+        body = {
+                "Name": self.uuid,
+                "Labels": {
+                    "Name": "sync-efs-to-s3"
+                },
+                "TaskTemplate": {
+                    "RestartPolicy": {
+                        "Condition": "on-failure",
+                        "Delay": 5000,
+                        "MaxAttempts": 3,
+                    },
+                    "ContainerSpec": {
+                        "Image": self.__getImage('sync-efs-to-s3'),
+                        "Env": [
+                            "AWS_ACCESS_KEY_ID=" + self.app_config['awscreds']['access_key'],
+                            "AWS_SECRET_ACCESS_KEY=" + self.app_config['awscreds']['secret_access_key'],
+                            "S3_REGION=" + dbItem['s3_region']['S'],
+                            "S3_BUCKET=" + dbItem['s3_bucket']['S'],
+                            "SITE_ID=" + query['siteId'],
+                            "SERVICE_NAME=" + self.uuid
+                        ],
+                        "Mounts": [{
+                            "Type": "volume",
+                            "Target": "/opt/efs/",
+                            "Source": query['fsId'] + "/" + query['siteId'],
+                            "VolumeOptions": {
+                                "DriverConfig": {
+                                    "Name": "efs"
+                                }
+                            }
+                        }]
+                    },
+                    "Placement": {
+                        "Constraints": ["node.labels.type == efs-worker"]
+                    },
+                }
+            }
+        return body
+
+    def __getWpServiceImageBody(self, query):
+        if 'phpVersion' not in query:
+            query['phpVersion'] = '7.0'
+        s3 = S3(self.app_config)
+        notification_url = s3.createNotificationUrl(self.notificationId)
+        env = [
+            "SERVICE_PORT=" + str(query['pubPort']),
+            "SITE_ID=" + query['siteId'],
+            "SERVICE_DOMAIN=" + self.app_config['service_domain'],
+            "EFS_ID=" + query['fsId'],
+            "NOTIFICATION_URL=" + base64.b64encode(notification_url)
+        ]
+        if 'wpArchiveId' in query:
+            archiveUrl = s3.createWpArchiceUrl(query['wpArchiveId'])
+            if archiveUrl is not False:
+                env.append('ARCHIVE_URL=' + base64.b64encode(archiveUrl))
+        body = {
+                "Name": query['siteId'],
+                "Labels": {
+                    "Name": "wordpress-worker"
+                },
+                "TaskTemplate": {
+                    "LogDriver": {
+                        "Name": "awslogs",
+                        "Options": {
+                            "awslogs-region": "us-east-1",
+                            "awslogs-group": "dockerlog-services",
+                            "awslogs-stream": query['siteId']
+                        }
+                    },
+                    "ContainerSpec": {
+                        "Image": self.__getImage('wordpress-worker', query['phpVersion']),
+                        "Env": env,
+                        "Mounts": [{
+                            "Type": "volume",
+                            "Target": "/var/www/html",
+                            "Source": query['fsId'] + "/" + query['siteId'] + "/web",
+                            "VolumeOptions": {
+                                "DriverConfig": {
+                                    "Name": "efs"
+                                }
+                            }
+                        },
+                        {
+                            "Type": "volume",
+                            "Target": "/var/lib/mysql",
+                            "Source": query['fsId'] + "/" + query['siteId'] + "/db",
+                            "VolumeOptions": {
+                                "DriverConfig": {
+                                    "Name": "efs"
+                                }
+                            }
+                        }]
+                    },
+                    "Placement": {
+                        "Constraints": ["node.labels.type == efs-worker"]
+                    },
+                },
+                "EndpointSpec": {
+                    "Ports": [
+                        {
+                            "Protocol": "tcp",
+                            "PublishedPort": int(query['pubPort']),
+                            "TargetPort": 443
+                        }
+                    ]
+                }
+        }
+        return body
