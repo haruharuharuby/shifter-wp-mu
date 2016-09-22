70971cbbbe254a0ed97125b93a2a9ecc160ff541
sawanoboly <sawanoboriyu@higanworks.com>
2016-09-22 18:16:21 +0900

    add ShifterExceptions.py for raise own purpose.

---
+++ mylibs/ShifterExceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+class ShifterErrorBase(Exception):
+    pass
+
+
+class ShifterUnknownError(ShifterErrorBase):
+    pass
