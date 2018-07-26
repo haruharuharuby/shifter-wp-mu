68630ecc9f779af78f6d4f9ccb1c0feb495ff35f
sawanoboly <sawanoboriyu@higanworks.com>
2018-07-26 16:40:36 +0900

    add _develop suffix for docker-image

--- mylibs/ServiceBuilder.py
+++ mylibs/ServiceBuilder.py
@@ -316,10 +316,17 @@ class ServiceBuilder:
             # return php_var_from_query or self.site_item.get('php_version', False) or 'latest'
             return self.site_item.get('php_version', False) or php_var_from_query or 'latest'
 
+        shifter_env = os.getenv("SHIFTER_ENV", 'development')
         context = {}
         context['service_name'] = self.query['siteId']
         context['service_type'] = __get_service_type_or_default()
-        context['image_string'] = ':'.join([self.app_config['docker_images']['wordpress-worker2'], __get_php_version_or_latest()])
+        if shifter_env == 'development':
+            tagname = __get_php_version_or_latest()
+            if not tagname.count('image'):
+                tagname = tagname + '_develop'
+                context['image_string'] = ':'.join([self.app_config['docker_images']['wordpress-worker2'], tagname])
+        else:
+            context['image_string'] = ':'.join([self.app_config['docker_images']['wordpress-worker2'], __get_php_version_or_latest()])
         context['publish_port1'] = int(self.query['pubPort'])
         context['efs_point_web'] = self.site_item['efs_id'] + "/" + self.query['siteId'] + "/web"
 
