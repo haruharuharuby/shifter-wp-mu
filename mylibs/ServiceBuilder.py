10f493429a0ae0a97c94a150e248336388a6efad
haruharuharuby <cocktail.house@gmail.com>
2020-02-23 02:14:59 +0100

    pass role name to container

--- mylibs/ServiceBuilder.py
+++ mylibs/ServiceBuilder.py
@@ -267,6 +267,7 @@ class ServiceBuilder:
             if self.query.get('refreshToken') and self.query.get('accessToken'):
                 env.append("SHIFTER_ACCESS_TOKEN=" + self.query['accessToken'])
                 env.append("SHIFTER_REFRESH_TOKEN=" + self.query['refreshToken'])
+                env.append("SHIFTER_USER_ROLE=" + self.query['role'])
                 env.append("SHIFTER_API_URL_V1=" + os.environ.get('SHIFTER_API_URL_V1'))
                 env.append("SHIFTER_API_URL_V2=" + os.environ.get('SHIFTER_API_URL_V2'))
                 env.append("SHIFTER_USER_EMAIL=" + self.query.get('email', ''))
